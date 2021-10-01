0b4efe215ccec40c4fe0845247225842f92c8e57
Ian Clark <IanClark2021@u.northwestern.edu>
2021-09-30 22:57:05 -0500

    Finalized Algorithm

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Define the generatePassword function seperately
 function generatePassword(){
+/* ________ Local Variables ________ */
+  // Const variable initialized to the value of the user num input 
+  let userNumInput = prompt('Select Desired Password Length, Min: 8, Max: 128', '48');
 
-// const variable for charsets 
+  // const variable for charsets 
   // const 1.
   const alphabet = 'abcedfghijklmnopqrstuvwxyz';
   // const 2.
@@ -10,18 +13,14 @@ function generatePassword(){
   const special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   // const 4.
   
-  // Const variable initialized to the value of the user num input 
-  let userNumInput = prompt('Select Desired Password Length, Min: 8, Max: 128');
 
-  // Variable Initialized to the value of user selected boolean. 
-  let userSelectLower = confirm('Include lowercase values?');
+  // variable initialized to the value of confirmation strings to be combined and displayed after password generation. 
+  let yesGenerated = 'Password generated: ';
+  const yesLower = 'added lowercase, ';
+  const yesUpper = 'added UPPER, ';
+  const yesSpecial = 'add3d sUm $P3C!@l, ';
+  const yesNumber = 'added some nums, ';
 
-  // Variable initialized 
-  let userSelectUpper = confirm('How bout Uppercase?');
-
-  let userSelectSpecial = confirm('Special Chars?');
-
-  const userSelectNum = confirm('Numerical Vals as Well?');
 
   // Starting Char Set initialized to the value of an empty string. 
   let startingChars = '';
@@ -29,35 +28,47 @@ function generatePassword(){
   // scrambled chars initialized to the value of an empty string 
   let scrambledChars = '';
 
-// Final Password initialized to the value of an empty string 
-var finalPassword = '';
+/* _________________    Conditions & Processes       ____________________ */
 
+// Main Conditional 
+  // If the user input does not meet the length requirement, we don't need to move forward.
+  if (isNaN(userNumInput) || userNumInput < 8 || userNumInput > 128 ) {
+    alert('Stop trying to break me!');
+  } else {
+      // If user input does meet requirements, Variables are Initialized to the value of user selected boolean. 
+    let userSelectLower = confirm('Include lowercase values?');
+  
+    let userSelectUpper = confirm('How bout Uppercase?');
+  
+    let userSelectSpecial = confirm('Special Chars?');
+  
+    const userSelectNum = confirm('Numerical Vals as Well?');
+  
+      // If the user selects a charset, add it to the value of startingCharSet & add the confirmation string to the final message. 
+      if (userSelectLower) {
+        startingChars += alphabet;
+        yesGenerated += yesLower;
+      } 
+    
+      if (userSelectUpper) {
+        startingChars += alphabet.toUpperCase();
+        yesGenerated += yesUpper;
+      }
+  
+      if (userSelectSpecial) {
+        startingChars += special;
+        yesGenerated += yesSpecial;
+      }
+  
+      if (userSelectNum) {
+        startingChars += numbers;
+        yesGenerated += yesNumber;      
+      }
+      // At the end of the else condition, display the yesGenerated confirm
+      confirm(yesGenerated);
+      
+    } 
 
-// Main Conditionals 
-
-  // Conditionals 
-// NaN Use for Validation 
-
-    // If the user selects a charset, add it to the value of startingCharSet
-  if (userSelectLower === true) {
-    startingChars += alphabet;
-  } 
-
-  if (userSelectUpper === true) {
-    startingChars += alphabet.toUpperCase();
-  }
-
-  if (userSelectSpecial === true) {
-    startingChars += special;
-  }
-
-  if (userSelectNum === true) {
-    startingChars += numbers;
-  }
-
-
-  // const random variable initialized to the value of 
-  // var generateRandomNum = Math.floor(Math.random() * startingChars.length);
 
 // Main Functionality 
 // 1. yates Scramble 
@@ -84,35 +95,13 @@ while (i-- > 0) {
 // Outside of the loop, convert the scrambled array into a string value. 
 scrambledChars = arrayCharacters.join('');
 
+
 // Slice the scrambledChars string beginning with 0 index, and ending at userInputNum 
 return scrambledChars.slice(0, userNumInput);
-
-
-  // while (finalPassword.length < userNumInput) {
-  //   var generateRandomNum = Math.floor(Math.random() * startingChars.length);
-  //   finalPassword += startingChars.charAt(generateRandomNum);
-  // }
-
 
 } 
 // End generatePassword 
 
-
-
-
-//  for (let i=finalPassword.length - 1; i<userNumInput; i++) {
-//   var generateRandomNum = Math.floor(Math.random() * startingChars.length);
-//   finalPassword += startingChars.charAt(generateRandomNum);
-//  }
-
-
-    // Return finalPassword to the global memory. 
-    // Which is then passed by the invocation of generatePassword within writePassword (but never returned out of the functions local memory)
-  
-    // Return
-  
-  
-  
   // Assignment Code
   var generateBtn = document.querySelector("#generate");
   
@@ -127,86 +116,6 @@ return scrambledChars.slice(0, userNumInput);
     passwordText.value = password;
   
   }
-  
-  
+
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
-  
-  
-
-  
-      
-  
-  
-  
-  
-  
-  // 1a. When answering prompts - Return Input validaton, selection of at least 1 character type
-  
-  // 1b. All prompts answered : Return Password according to criteria
-  
-  // 1c. On Password generation
-  // What data types need to be defined 
-  
-  // What processes do we want to interact with those data types
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-    // Psuedo Acceptance Criteria
-  
-  // 1. On click, Password Generator
-    // 2. Criteria Prompt - Select Criteria to Include in Password
-  
-    // 3. Length Prompt - Choose Preferred Length (between 8 - 128 chars) 
-        // Data Type = Number From 8 to 128
-            // const userLength = prompt('please choose a password length. min: 8, max: 128.');
-          // Defines output length
-          // Alert if {low}
-          // Alert: else if {typeof userLength !== 'Number'}
-          // Alert else if {high}
-          // Else {No alert: save the preffered num as variable we can use later}
-          
-        // let charSetNum = 0;
-  
-          // Use userLength Var to then,
-             // 4. Char Type Processes - Select lowercase, uppercase, numeric, special chars
-                
-                    // Char Types = Array of Variables?
-                      // [lowercase, uppercase, numeric, special chars]
-  
-                      // If lowercaseConfirm === true 
-                            // Then, validate user resonse 
-                            // Then, charSetNum++
-  
-                      // If uppercaseConfirm === true 
-                            // Then, validate user resonse 
-                            // Then, lowerCaseString.toUpperCase()
-                            // Then, charSetNum++
-  
-                      // If specialCharConfirm === true
-                            // Then, validate user resonse 
-                            // Then, charSetNum++
-                      
-                // Make numericalCharConfirm the last Prompt of its kind, we can then check the value of charSetNum, validating that its value is strictly Greater or Equal to 1 
-  
-                      // If numericalCharConfirm === false && charSetNum < 1 
-                            // Then, Throw an alert
-                      // In every other case, where the numericalCharConfirm is true. 
-                          // Then, charSetNum++
-                      
-  
-                    
-  
-                            // 
-                        
-                    // In case user selects 1 character type
-      // 
-   
-      // 
